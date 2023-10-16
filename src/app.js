cef6623aa123fa503fe10fc6060f7ec467444e99
jant7070 <50752064+jant7070@users.noreply.github.com>
2023-10-16 23:46:36 +0000

    se agrego el innerHTML para mostrat la excusa

--- src/app.js
+++ src/app.js
@@ -8,19 +8,26 @@ import "./assets/img/4geeks.ico";
 window.onload = function() {
   //write your code here
   function excuseGenerator() {
-    let who = ['The dog', 'My grandma', 'His turtle', 'My bird'];
-    let action = ['ate', 'peed', 'crushed', 'broke'];
-    let what = ['my homework', 'the keys', 'the car'];
-    let when = ['before the class', 'right on time', 'when I finished', 'during my lunch', 'while I was praying'];
+    let who = ["The dog", "My grandma", "His turtle", "My bird"];
+    let action = ["ate", "peed", "crushed", "broke"];
+    let what = ["my homework", "the keys", "the car"];
+    let when = [
+      "before the class",
+      "right on time",
+      "when I finished",
+      "during my lunch",
+      "while I was praying"
+    ];
 
-    let quien = who[Math.floor(Math.random() * 4)]
-    let accion = action[Math.floor(Math.random() * 4)]
-    let que = what[Math.floor(Math.random() * 3)]
-    let cuando = when[Math.floor(Math.random() * 5)]
+    let quien = who[Math.floor(Math.random() * 4)];
+    let accion = action[Math.floor(Math.random() * 4)];
+    let que = what[Math.floor(Math.random() * 3)];
+    let cuando = when[Math.floor(Math.random() * 5)];
 
-    return `${quien} ${accion} ${que} ${cuando}`
-}
+    return `${quien} ${accion} ${que} ${cuando}`;
+  }
 
-console.log(excuseGenerator())
+  console.log(excuseGenerator());
 
+  document.getElementById("demo").innerHTML = excuseGenerator();
 };

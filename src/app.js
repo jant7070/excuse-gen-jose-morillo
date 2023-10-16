07177641e73b40cec34ac25cf41b4be4490a4f5b
jant7070 <50752064+jant7070@users.noreply.github.com>
2023-10-16 23:24:18 +0000

    cambios excuse generator

--- src/app.js
+++ src/app.js
@@ -7,5 +7,20 @@ import "./assets/img/4geeks.ico";
 
 window.onload = function() {
   //write your code here
-  console.log("Hello Rigo from the console!");
+  function excuseGenerator() {
+    let who = ['The dog', 'My grandma', 'His turtle', 'My bird'];
+    let action = ['ate', 'peed', 'crushed', 'broke'];
+    let what = ['my homework', 'the keys', 'the car'];
+    let when = ['before the class', 'right on time', 'when I finished', 'during my lunch', 'while I was praying'];
+
+    let quien = who[Math.floor(Math.random() * 4)]
+    let accion = action[Math.floor(Math.random() * 4)]
+    let que = what[Math.floor(Math.random() * 3)]
+    let cuando = when[Math.floor(Math.random() * 5)]
+
+    return `${quien} ${accion} ${que} ${cuando}`
+}
+
+console.log(excuseGenerator())
+
 };
